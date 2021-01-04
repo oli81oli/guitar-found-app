902f1bc0ccd6a525480c558ceeec6877410941d0
Oliver <oli.b.alonso@gmail.com>
2021-01-04 22:26:02 +0100

    to deploy

--- configs/middleware.config.js
+++ configs/middleware.config.js
@@ -3,7 +3,7 @@ const cookieParser = require('cookie-parser')
 const logger = require('morgan')
 const cors = require('cors')
 
-const whitelist = [ process.env.DOMAIN_REMOTE, process.env.DOMAIN_LOCAL]
+const whitelist = ['https://jovial-wilson-b4e0fe.netlify.app/', process.env.DOMAIN_REMOTE, process.env.DOMAIN_LOCAL]
 const corsOptions = {
     origin: (origin, cb) => {
         const originIsWhitelisted = whitelist.includes(origin)
